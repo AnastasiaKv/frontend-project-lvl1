2e926cd2c35d8d8ee8a42ae3623113b4e0035dc2
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-02-10 10:46:03 +0300

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import interactiveGreeting from '../src/cli.js';
-import ifEvenGame from '../src/ifEven.js';
+import ifEvenGame from '../src/games/ifEven.js';
 
 const userName = interactiveGreeting();
 ifEvenGame(userName);
