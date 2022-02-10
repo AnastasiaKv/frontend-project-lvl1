f3491ce885a561d62d7e550ed866d16c349e0a42
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-02-10 12:33:18 +0300

    add gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,23 @@
+import * as game from '../index.js';
+
+const instruction = 'Find the greatest common divisor of given numbers.';
+const maxNumber1 = 30;
+const maxNumber2 = 100;
+
+const getNumbers = () => [game.random(maxNumber1 + 1), game.random(maxNumber2 + 1)];
+const numbersToString = (nums) => `${nums[0]} ${nums[1]}`;
+const findGSD = (a, b) => (!b ? Math.abs(a) : findGSD(b, a % b));
+
+const gcdGame = (userName) => {
+  let score = 0;
+  game.init(instruction);
+
+  do {
+    const nums = getNumbers();
+    score = game.round(numbersToString(nums), findGSD(...nums), score);
+  } while (game.checkScore(score));
+
+  game.finish(score, userName);
+};
+
+export default gcdGame;
