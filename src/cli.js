08fd6a9fe95210eec7ba97972aded77b88b4d7ce
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-02-15 16:26:00 +0300

    fix: add default value to userName

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello${userName ? `, ${userName}` : ''}!`);
+  const userName = readlineSync.question('May I have your name? ') || 'Anonym';
+  console.log(`Hello, ${userName}!`);
   return userName;
 };
